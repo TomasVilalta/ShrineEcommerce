c30948656c323e69537ca04480b32b4aa88b2889
TomasVilalta <tomasfedericovilalta@gmail.com>
2022-11-03 16:53:31 -0300

    Added navigation

--- app/src/main/java/com/example/tryingout/LoginFragment.kt
+++ app/src/main/java/com/example/tryingout/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.tryingout.databinding.FragmentLoginBinding
 
 
@@ -23,6 +25,6 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.loginButton.setOnClickListener{}
+        binding.loginButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_homeFragment))
     }
 }
