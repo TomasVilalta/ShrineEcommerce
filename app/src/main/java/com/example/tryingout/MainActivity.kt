6f187b4967548dfd8d221f5c99e99f0e31435fe8
TomasVilalta <tomasfedericovilalta@gmail.com>
2022-11-03 10:44:49 -0300

    First commit

---
+++ app/src/main/java/com/example/tryingout/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.tryingout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.tryingout.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+}
