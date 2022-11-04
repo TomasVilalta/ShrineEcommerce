ecbfa10311ec11462b876c1c1abf1b85012d5ae5
TomasVilalta <tomasfedericovilalta@gmail.com>
2022-11-04 09:17:49 -0300

    Added Toolbar

--- app/src/main/java/com/example/tryingout/HomeFragment.kt
+++ app/src/main/java/com/example/tryingout/HomeFragment.kt
@@ -1,20 +1,43 @@
 package com.example.tryingout
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import com.example.tryingout.databinding.FragmentHomeBinding
+import com.example.tryingout.databinding.FragmentLoginBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        // Toolbar Setup
+        (activity as AppCompatActivity).setSupportActionBar(binding.appBar)
+        (requireActivity() as MenuHost).addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu, menu)
 
+            }
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                when (menuItem.itemId) {
+                    R.id.menu_search -> {
+                        Toast.makeText(activity, "hola", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+                return true
+            }
+        }, viewLifecycleOwner)
+        return binding.root
+    }
 
 }
